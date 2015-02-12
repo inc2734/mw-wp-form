b0329fa58860be99e0e1cdedffa883f2b029cfce
Takashi Kitajima <inc@2inc.org>
2015-02-12 20:12:16 +0900

    fix typo

--- classes/form-fields/class.checkbox.php
+++ classes/form-fields/class.checkbox.php
@@ -121,7 +121,7 @@ class MW_WP_Form_Field_Checkbox extends MW_WP_Form_Abstract_Form_Field {
 			<strong><?php esc_html_e( 'Send value by e-mail', MWF_Config::DOMAIN ); ?></strong>
 			<?php $value = $this->get_value_for_generator( 'value', $options ); ?>
 			<?php $post_raw = $this->get_value_for_generator( 'post_raw', $options ); ?>
-			<label><input type="checkbox" name="post_raw" value="false" <?php checked( 'false', $post_raw ); ?> /> <?php esc_html_e( 'Send post value when you split tha post value and display value by ":" in choices.', MWF_Config::DOMAIN ); ?></label>
+			<label><input type="checkbox" name="post_raw" value="false" <?php checked( 'false', $post_raw ); ?> /> <?php esc_html_e( 'Send post value when you split the post value and display value by ":" in choices.', MWF_Config::DOMAIN ); ?></label>
 		</p>
 		<p>
 			<strong><?php esc_html_e( 'Default value', MWF_Config::DOMAIN ); ?></strong>
