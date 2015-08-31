6fffb9ec1388b2e7b4bf93839f9137a4f7a8e136
ozonenotes Kei Nomura <kei@ozonenotes.jp>
2015-09-01 00:25:53 +0900

    Add validation rule "Japanese Hiragana or Katakana"

---
+++ classes/validation-rules/class.kana.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Name       : MW WP Form Validation Rule Kana
+ * Description: 値がひらがな or カタカナ
+ * Version    : 1.1.1
+ * Author     : Takashi Kitajima
+ * Author URI : http://2inc.org
+ * Created    : July 21, 2014
+ * Modified   : April 1, 2015
+ * License    : GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+class MW_WP_Form_Validation_Rule_Kana extends MW_WP_Form_Abstract_Validation_Rule {
+
+	/**
+	 * バリデーションルール名を指定
+	 * @var string
+	 */
+	protected $name = 'kana';
+
+	/**
+	 * バリデーションチェック
+	 *
+	 * @param string $key name属性
+	 * @param array $option
+	 * @return string エラーメッセージ
+	 */
+	public function rule( $key, array $options = array() ) {
+		$value = $this->Data->get( $key );
+		if ( !MWF_Functions::is_empty( $value ) ) {
+			if ( !preg_match( '/^[ぁ-んァ-ヶー]+$/u', $value ) ) {
+				$defaults = array(
+					'message' => __( 'Please enter with a Japanese Hiragana or Katakana.', MWF_Config::DOMAIN )
+				);
+				$options = array_merge( $defaults, $options );
+				return $options['message'];
+			}
+		}
+	}
+
+	/**
+	 * 設定パネルに追加
+	 *
+	 * @param numeric $key バリデーションルールセットの識別番号
+	 * @param array $value バリデーションルールセットの内容
+	 */
+	public function admin( $key, $value ) {
+		?>
+		<label><input type="checkbox" <?php checked( $value[$this->getName()], 1 ); ?> name="<?php echo MWF_Config::NAME; ?>[validation][<?php echo $key; ?>][<?php echo esc_attr( $this->getName() ); ?>]" value="1" /><?php esc_html_e( 'Japanese Hiragana or Katakana', MWF_Config::DOMAIN ); ?></label>
+		<?php
+	}
+}
