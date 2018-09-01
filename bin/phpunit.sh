72769c15be2211fab118e2e40803e11c838df2dd
Takashi Kitajima <inc@2inc.org>
2018-09-01 13:45:24 +0900

    Some fixes

---
+++ bin/phpunit.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ -e /tmp/wordpress-tests-lib ]; then
+  plugindir=$(pwd)
+
+  cd ${plugindir};
+  vendor/bin/phpunit
+  exit 0
+fi
+
+dir=$(cd $(dirname $0) && pwd)
+bash "${dir}/wpphpunit.sh"
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,8 +6,14 @@
  */
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
+
 if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
+
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL;
+	exit( 1 );
 }
 
 // Give access to tests_add_filter() function.
@@ -23,35 +29,3 @@ tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
-
-if ( ! function_exists( '_delete_all_data' ) ) {
-	function _delete_all_data() {
-		global $wpdb;
-
-		$tables = array(
-			'posts', 'postmeta', 'comments', 'commentmeta', 'term_relationships', 'termmeta',
-		);
-		$wptables = array();
-		foreach ( $tables as $table ) {
-			if ( isset( $wpdb->$table ) ) {
-				$wptables[] = $wpdb->$table;
-			}
-		}
-
-		foreach ( $wptables as $table ) {
-			$wpdb->query( "DELETE FROM {$table}" );
-		}
-
-		foreach ( array(
-			$wpdb->terms,
-			$wpdb->term_taxonomy
-		) as $table ) {
-			$wpdb->query( "DELETE FROM {$table} WHERE term_id != 1" );
-		}
-
-		$wpdb->query( "UPDATE {$wpdb->term_taxonomy} SET count = 0" );
-
-		$wpdb->query( "DELETE FROM {$wpdb->users} WHERE ID != 1" );
-		$wpdb->query( "DELETE FROM {$wpdb->usermeta} WHERE user_id != 1" );
-	}
-}
