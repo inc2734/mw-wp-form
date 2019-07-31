e051c5fcce862c6aa8f3b5b1e2af46e91d5f73cd
Takashi Kitajima <inc@2inc.org>
2019-07-31 10:31:41 +0900

    Fix #97

--- classes/deprecated.php
+++ classes/deprecated.php
@@ -2,11 +2,15 @@
 class MWF_Deprecated {
 
 	public function __construct() {
-		add_action( 'mwform_after_exec_shortcode', array( $this, '_mwform_after_exec_shortcode' ), 10000 );
+		add_action( 'mwform_after_exec_shortcode', array( $this, '_mwform_after_exec_shortcode2' ), 10000 );
 	}
 
-	public function _mwform_after_exec_shortcode() {
-		if ( has_filter( 'mwform_after_exec_shortcode' ) ) {
+	public function _mwform_after_exec_shortcode2() {
+		global $wp_filter;
+
+		remove_action( 'mwform_after_exec_shortcode', array( $this, '_mwform_after_exec_shortcode2' ), 10000 );
+
+		if ( has_action( 'mwform_after_exec_shortcode' ) ) {
 			MWF_Functions::deprecated_message(
 				'mwform_after_exec_shortcode',
 				'mwform_start_main_process'
@@ -14,4 +18,5 @@ class MWF_Deprecated {
 		}
 	}
 }
+
 new MWF_Deprecated();
